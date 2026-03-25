9d5bc094b60d12d94798ac1ce973862c7a7a6f1d
jkay2005 <23520729@gm.uit.edu.vn>
2026-03-26 01:46:35 +0700

    update text, cicd basic

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Backend Node.js đang chạy thành công qua Docker! Database và Redis đã sẵn sàng.');
+  res.send('Hoàn thành CI/CD pipeline trên GitHub Actions!');
 });
 
 app.listen(port, () => {
