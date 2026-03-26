958dda6b8f671cb469aa6c8c8952636b8f11a9b3
jkay2005 <23520729@gm.uit.edu.vn>
2026-03-27 02:00:24 +0700

    Tích hợp Trivy quét lỗ hổng Docker Image-lan 2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello từ AKS tự động hóa hoàn toàn!');
+  res.send('Hello từ AKS, đang test Trivy!');
 });
 
 app.listen(port, () => {
